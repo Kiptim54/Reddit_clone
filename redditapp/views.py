454753e203297ab8271c1275ee5dc54155367fd9
Kiptim <kiptim54@gmail.com>
2018-07-02 11:17:01 +0300

    add set up home page

--- redditapp/views.py
+++ redditapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    title="Home Page | Reddit"
+    return HttpResponse("Hey there I already forgot this in a week can you believe me!")
